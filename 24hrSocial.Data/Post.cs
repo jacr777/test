33006c710b1fa23a161da9f69f037642fd399ed5
Antonio <jacr777@gmail.com>
2020-10-22 20:58:25 -0400

    6 commit

---
+++ 24hrSocial.Data/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24hrSocial.Data
+{
+    public class Post
+    {
+        [key]
+        public int PostId { get; set; }        
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(ApplicationUser))]
+        public string Id { get; set; }
+        public virtual ApplicationUser User { get; set; }
+     
+   
+    }
+}
---
+++ 24hrSocial.Data/Reply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24hrSocial.Data
+{
+    public class Reply
+    {
+        /// This is just a test please ignore
+    }
+}
